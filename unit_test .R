b3ef53176d7585f1bcc6c14d1bff85dc9a79cd15
Mateus  Maia <mateusmaia@Mateuss-MacBook-Pro.local>
2022-02-04 14:00:52 +0000

    Adding minor examples

--- unit_test .R
+++ unit_test .R
@@ -1,3 +1,6 @@
+# This file is just to measure if the main code for BART is running well, isn't made for 
+#evaluation purposes.
+
 # Importing data and library
 rm(list=ls())
 library(mlbench)
@@ -21,7 +24,6 @@ bart_mod <- bart(x = x_train,y = y_train,number_trees = 20,
 
 bart_pred <- predict_bart(bart_mod = bart_mod, newdata = x_test,type = "mean")
 
-crossprod( (bart_pred-y_test))
 
 x_train <- cross_validation_object[[1]]$x_train
 y_train <- cross_validation_object[[1]]$y_train
@@ -32,5 +34,7 @@ dbart_mod <- dbarts::bart(x.train = x_train,
              ntree = 20,keeptrees = TRUE)
 
 crossprod( (y_test- predict(dbart_mod,newdata = x_test) %>% apply(2,mean)))
+crossprod( (bart_pred-y_test))
+
 bart_pred
 y_test
--- unit_test_gpbart.R
+++ unit_test_gpbart.R
@@ -1,9 +1,20 @@
+# This file is just to measure if the main code for GP-BART is running well, isn't made for 
+#evaluation purposes.
+
+# Setting a seeed
+set.seed(42)
+
+
 # Importing data and library
 rm(list=ls())
 library(mlbench)
+library(Rcpp)
+
 source("gpbart.R")
 source("bart.R")
 source("tree_manipulation_objects.R")
+sourceCpp("dist_matrix.cpp")
+source("fast_gp_multiple_tau.R")
 
 # Getting a simple train and test dataset
 train_data <- mlbench.friedman1(n = 50,sd = 1)
@@ -14,23 +25,23 @@ test_data <- mlbench.friedman1(n = 10, sd = 1)
 x_test <- test_data$x
 y_test <- test_data$y
 colnames(x_train) <- colnames(x_test) <- paste0("x.",1:ncol(x_train))
+
+# Running the model
 gpbart_mod <- my_rBart_gp(x = x_train,
                           y = y_train,
-                          number_trees = 20,node_min_size = 5,kappa = 1,
-                          n_iter = 2000,bart_number_iter = 2000)
+                          number_trees = 20,node_min_size = 5,kappa = 0.5,
+                          n_iter = 2000,alpha = 0.5,beta = 20,scale_boolean = TRUE)
 
-gpbart_mod$nu_store<- matrix(1e-10, nrow = nrow(gpbart_mod$nu_store),ncol = ncol(gpbart_mod$nu_store))
 
-pred_gpbart <- my_predict_rBART(rBart_model = gpbart_mod, x_test = x_test,type  = c("mean"),pred_bart_only = TRUE)
-crossprod( (pred_gpbart$out$pred-y_test))
+# Running the prediction
+pred_gpbart <- my_predict_rBART(rBart_model = gpbart_mod, x_test = x_test,type  = c("mean"))
 
-# x_train <- cross_validation_object[[1]]$x_train
-# y_train <- cross_validation_object[[1]]$y_train
-# x_test <- cross_validation_object[[1]]$x_test
-
+# Running the standard from dBARTs
 dbart_mod <- dbarts::bart(x.train = x_train,
                           y.train = y_train,
                           ntree = 20,keeptrees = TRUE)
+
+crossprod( (pred_gpbart$out$pred-y_test))
 
 crossprod( (y_test- predict(dbart_mod,newdata = x_test) %>% apply(2,mean)))
 
@@ -38,6 +49,3 @@ pred_gpbart$out$pred
 predict(dbart_mod,newdata = x_test) %>% apply(2,mean)
 y_test
 
-
-points(x_test[,1],pred_gpbart$list_matrix_pred[[1]][1,], pch = 20, col = "blue")
-plot(x_train[,1],gpbart_mod$current_partial_residuals_list[[1]][1,],pch = 20)
