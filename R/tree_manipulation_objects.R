760ef5c0014d98a4da12f5ae260cd5f2011db82d
MateusMaiaDS <mmateusmaiads@gmail.com>
2022-05-27 15:34:25 +0100

    fixing the transition

--- R/tree_manipulation_objects.R
+++ R/tree_manipulation_objects.R
@@ -828,6 +828,7 @@ log_transition_prob <- function(current_tree, new_tree, verb){
                 grow_projection = (log(p_prune)+log(get_leaves(current_tree)))-(log(get_branches(new_tree))+ log(p_grow)),
                 prune = (log(p_grow)+log(get_branches(current_tree)))-(log(get_leaves(new_tree))+ log(p_prune)),
                 change = 0,
+                change_projection = 0,
                 swap = 0 ))
 }
 
