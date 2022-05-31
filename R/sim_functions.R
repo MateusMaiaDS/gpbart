ea82f0660176cbda00e4a650efc5780fc8daad49
MateusMaiaDS <mmateusmaiads@gmail.com>
2022-05-31 11:13:00 +0100

    Adding new simulations scenarios

--- R/sim_functions.R
+++ R/sim_functions.R
@@ -30,8 +30,8 @@ two_d_sim_two_node_gp_sum_each_tree <- function(n, # Number of observations
   
   
   # Generating the x axis
-  x <- expand.grid(lat = seq(-1,1,length.out = round(sqrt(n))),
-                   lon = seq(-1,1,length.out = round(sqrt(n))))
+  x <- expand.grid(lat = seq(-10,10,length.out = round(sqrt(n))),
+                   lon = seq(-10,10,length.out = round(sqrt(n))))
   
   # Creating the true response
   y <- numeric(nrow(x))
@@ -150,3 +150,59 @@ two_d_sim_two_node_gp_sum_each_tree <- function(n, # Number of observations
   return(as.matrix(data.frame(x, y = y)))
   
 }
+
+# New simulation scenario
+sim_2d_two_node <- function(n, seed, sd_value){
+
+  
+  # Setting a seed
+  
+  # Creating x and y
+  x <- replicate(stats::runif(n = n,min = -1,max = 1),n = 2)
+  colnames(x) <- c("lon","lat")
+  y <- numeric(n)
+  
+  # Getting the left and right nodes
+  left_index <- which(x[,"lon"]<0)
+  right_index <- which(x[,"lon"]>=0)
+  
+  y[left_index] <- exp( (-0.5(x[left_index,"lon"]^2+x[left_index,"lat"]^2)))
+  y[right_index] <- -exp( (-0.5(x[right_index,"lon"]^2+x[right_index,"lat"]^2)))
+  y <- y + stats::rnorm(sd = sd_value,n = n)
+  sim_data <- cbind(x,y)
+  
+  return(sim_data)  
+}
+
+# spatial_model <- sim_2d_two_node(n = 100,seed = 42,sd_value = 0.1)
+# x <- spatial_model[,1:2,drop = FALSE]
+# y <- spatial_model[,3,drop = FALSE]
+# 
+# # Test for spatial model
+# spatial_model_test <- sim_2d_two_node(n = 200,seed = 43,sd_value = 0.1)
+# x_test <- spatial_model_test[,1:2,drop = FALSE]
+# y_test <- spatial_model_test[,3,drop = FALSE]
+# 
+# 
+# bart_mod <- dbarts::bart(x.train = x,y.train = y,keeptrees = TRUE)
+# bart_pred <- predict(bart_mod,newdata = x_test)
+# 
+# bart_mod$sigma %>% hist
+# 
+# soft_bart <- SoftBart::softbart(X = x,Y = y,X_test = x_test)
+# soft_bart$sigma %>% hist()
+# 
+# gpbart_mod <- gp_bart(x = x,y = y,number_trees = 10)
+# gpbart_pred <- predict(gpbart_mod,x_test = x_test)
+# 
+# gpbart_pred$out$sd %>% hist
+# 
+# crps(y = y,means = colMeans(gpbart_pred$out$pred),sds = rep(mean(gpbart_pred$out$sd),100))$CRPS
+# crps(y = y,means = colMeans(bart_mod$yhat.train),sds = rep(mean(bart_mod$sigma),100))$CRPS
+# crps(y = y,means = colMeans(soft_bart$y_hat_train),sds = rep(mean(soft_bart$sigma),100))$CRPS
+# 
+# rmse(obs = y_test,pred = colMeans(bart_pred))
+# rmse(obs = y_test,pred = colMeans(gpbart_pred$out$pred))
+# rmse(obs = y_test,pred = colMeans(soft_bart$y_hat_test))
+# 
+# plot3d(cbind(x_test,y_test))
