20ea92e6414b597615de97e97a7ec674240491b8
MateusMaiaDS <mmateusmaiads@gmail.com>
2022-06-03 14:59:31 +0100

    modifing the PI coverage

--- R/gpbart.R
+++ R/gpbart.R
@@ -1799,25 +1799,38 @@ get_train_predictions <- function(gpbart_mod) {
 
 # Calculating a PI coverage
 #' @export
-pi_coverage <- function(y, y_hat_post, sd_post,only_post = FALSE, prob = 0.5){
+pi_coverage <- function(y, y_hat_post, sd_post,only_post = FALSE, prob = 0.5,n_mcmc_replications = 1000){
   
-    # Getting the number of posterior samples and columns, respect.
-    np <- nrow(y_hat_post)
-    nobs <- ncol(y_hat_post)
+  # Getting the number of posterior samples and columns, respect.
+  np <- nrow(y_hat_post)
+  nobs <- ncol(y_hat_post)
   
-    # Only post matrix
-    if(only_post){
-      post_draw <- y_hat_post
-    } else {
-      post_draw <- y_hat_post + replicate(sd_post,n = nobs)*matrix(stats::rnorm(n = np*nobs),
-                                                                 nrow = np)
+  full_post_draw <- list()
+  
+  # Setting the progress bar
+  progress_bar <- utils::txtProgressBar(
+    min = 1, max = n_mcmc_replications,
+    style = 3, width = 50 )
+  
+  # Only post matrix
+  if(only_post){
+    post_draw <- y_hat_post
+  } else {
+    for(i in 1:n_mcmc_replications){
+      utils::setTxtProgressBar(progress_bar, i)
+      
+      full_post_draw[[i]] <-(y_hat_post + replicate(sd_post,n = nobs)*matrix(stats::rnorm(n = np*nobs),
+                                                                             nrow = np))
     }
-    
-    # CI boundaries
-    low_ci <- apply(post_draw,2,function(x){stats::quantile(x,probs = prob/2)})
-    up_ci <- apply(post_draw,2,function(x){stats::quantile(x,probs = 1-prob/2)})
+  }
   
-    pi_cov <- sum((y<=up_ci) & (y>=low_ci))/length(y)
+  post_draw<- do.call(rbind,full_post_draw)
+  
+  # CI boundaries
+  low_ci <- apply(post_draw,2,function(x){stats::quantile(x,probs = prob/2)})
+  up_ci <- apply(post_draw,2,function(x){stats::quantile(x,probs = 1-prob/2)})
+  
+  pi_cov <- sum((y<=up_ci) & (y>=low_ci))/length(y)
   
   return(pi_cov)
 }
