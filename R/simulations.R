965831496df730c425e5e3618d4636b049252056
MateusMaiaDS <mmateusmaiads@gmail.com>
2022-12-17 12:23:13 +0100

    Adding Andrew friedman simulation scenario

--- R/simulations.R
+++ R/simulations.R
@@ -20,3 +20,26 @@ sim_friedman <- function(n,p = 10,seed,sd){
 }
 
 
+#' @export
+sim_friedman_andrew <- function(n,seed,sd){
+        
+        # Setting a seed if necessary
+        set.seed(seed)
+        
+        # Getting the X matrix
+        x <- matrix(stats::runif(2* n), ncol = 2)
+        
+        y <- 20*(x[,1]-0.5)^2
+        
+        # Adding the noise
+        y <- y + stats::rnorm(n = n,sd = sd)
+        
+        sim_data <- cbind(x,y)
+        colnames(sim_data) <- c(paste0("x.",1:2),"y")
+        
+        # Returning the simulated data 
+        return(sim_data)
+}
+
+
+
